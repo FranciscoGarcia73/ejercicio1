0a3f45a46f06e417ece7df1955665943ca175d28
Fran <frangarlop@gmail.com>
2021-03-08 22:18:13 +0100

    cambio version asincrona

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const chalk = require('chalk');
 
 // VERSIÓN ASÍNCRONA:
 
-// fs.readdir('./ficheros', (err, files) => {
-//     if (err) return console.log(err);
-//     console.log(files);
-//     let archivosFiltrados = files.filter(archivo => archivo.includes(".md"));
-//     console.log(chalk.green(archivosFiltrados));
-//     for (let archivo of archivosFiltrados) {
-//         fs.readFile(`./ficheros/${archivo}`, 'utf-8', (err, data) => {
-//             if (err) return console.log(err);
-//             console.log(chalk.magentaBright(data));
-//         });
-//     }
-// });
+fs.readdir('./ficheros', (err, files) => {
+    if (err) return console.log(err);
+    console.log(files);
+    let archivosFiltrados = files.filter(archivo => archivo.includes(".md"));
+    console.log(chalk.green(archivosFiltrados));
+    for (let archivo of archivosFiltrados) {
+        fs.readFile(`./ficheros/${archivo}`, 'utf-8', (err, data) => {
+            if (err) return console.log(err);
+            console.log(chalk.magentaBright(data));
+        });
+    }
+});
 
 // // VERSIÓN ASÍNCRONA v2:
 
@@ -38,14 +38,14 @@ const chalk = require('chalk');
 
 // // VERSIÓN SÍNCRONA:
 
-const files = fs.readdirSync('./ficheros');
-console.log(files);
-let archivosFiltrados = files.filter(archivo => archivo.includes(".md"));
-console.log(chalk.green(archivosFiltrados));
-for (let archivo of archivosFiltrados) {
-    const data = fs.readFileSync(`./ficheros/${archivo}`, 'utf-8');
-    console.log(chalk.magentaBright(data) + " \n de forma sincrona");
-};
+// const files = fs.readdirSync('./ficheros');
+// console.log(files);
+// let archivosFiltrados = files.filter(archivo => archivo.includes(".md"));
+// console.log(chalk.green(archivosFiltrados));
+// for (let archivo of archivosFiltrados) {
+//     const data = fs.readFileSync(`./ficheros/${archivo}`, 'utf-8');
+//     console.log(chalk.magentaBright(data) + " \n de forma sincrona");
+// };
 
 
 
